f876165b32353500faa963f4630e8096cb3c612a
koichi.sakamoto <sakamoto0321@gmail.com>
2022-05-13 16:07:37 +0900

    add files

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const uploads3  = require('./uploads3.js');
+require('date-utils');
+
+(async () => {
+  const target_dir = process.argv[2]
+  const lockfile = `${target_dir}/make.now`
+
+  if( fs.existsSync(lockfile)) {
+    return;
+  }
+  fs.writeFileSync(lockfile, new Date().toFormat("YYYY/MM/DD HH24:MI:SS"))
+  const dirList = uploads3.getMovieDirs(target_dir)
+  for(let dir of dirList) {
+    try {
+      console.log(dir)
+      const params = uploads3.getParameters(dir)
+      for(pm of params) {
+        await uploads3.upload(dir, pm)
+      }
+    }catch(e) {
+      console.log(e)
+    }
+  }
+  fs.unlinkSync(lockfile)
+})()
---
+++ uploads3.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require("glob");
+const { spawn, exec } = require('child_process')
+require('date-utils');
+
+function createConcatFileList(dir) {
+  const fileList = `${dir}/files.txt`
+  const files = glob.sync(`*.MP4`, {cwd:dir})
+  const stream = fs.createWriteStream(fileList)
+  files.forEach(file => stream.write(`file ${file}\n`))
+  stream.close()
+  return fileList
+}
+
+exports.getMovieDirs = function (targetDir) {
+  const dirs = glob.sync( `*/`, {cwd:targetDir, absolute:true} )
+  return dirs.filter(dir => ! fs.existsSync( `${dir}/files.txt` ) && 0 < glob.sync( `${dir}/*.[Mm][Pp]4` ).length)
+}
+
+exports.getParameters = function (dir) {
+  const movies = glob.sync(`*.[Mm][Pp]4`, {cwd:dir})
+
+  const fileList = `${dir}/files.txt`
+  const stream = fs.createWriteStream(fileList)
+  movies.forEach(file => stream.write(`file ${file}\n`))
+  stream.close()
+
+  const ret = movies.map(file => createCommandParam(dir, file))
+  ret.push(createCommandParam(dir, 'files.txt'))
+
+  return ret
+}
+
+function createCommandParam(dir, file) {
+  const localPath = `${dir}/${file}`
+  const s3Path = `s3://movies123/${path.basename(dir)}/${file}`
+  const param = []
+  param.push('s3')
+  param.push('cp')
+  param.push(localPath)
+  param.push(s3Path)
+  return param
+}
+
+exports.upload = function (dir, param) {
+  const LOG = `${dir}/upload.log`  
+  function log(msg) {
+    const now = new Date().toFormat("YYYY/MM/DD HH24:MI:SS")
+    msg.split("\n").forEach(line => {
+      fs.appendFileSync(LOG, `${now} ${line}\n`)
+    })
+  }
+
+  return new Promise(function (resolve, reject) {
+    const s3Path = param[param.length-1]
+    log(`start ${s3Path}`)
+    const proc = spawn('aws', param)
+    proc.stdout.on('data', (chunk) => console.log(chunk.toString()))
+    proc.stderr.on('data', (chunk) => console.log(chunk.toString()))
+  
+    proc.on('close', (code) => {
+      log(`close ${s3Path}`)
+      resolve(code);
+    })
+    process.on('error', function (err) {
+      log(`error ${s3Path}`)
+      reject(err);
+    })
+  })
+}
